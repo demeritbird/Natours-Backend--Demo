e98f49894d7c2e7f8e230c3c8dbcc7da7507127d
demeritbird <lekjiewei@hotmail.sg>
2022-07-10 14:14:04 +0800

    add middleware & morgan

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-const { create } = require('domain');
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const fs = require('fs');
+
+app.use(morgan('dev'));
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware!');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
+
   const id = req.params.id * 1;
   if (id > tours.length) {
     return res.status(404).json({
@@ -22,6 +36,7 @@ const getAllTours = (req, res) => {
 
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tour: tour,
@@ -30,7 +45,7 @@ const getAllTours = (req, res) => {
 };
 
 const getTour = (req, res) => {
-  const id = +req.params;
+  const id = +req.params.id;
   const tour = tours.find((el) => el.id === id);
   if (!tour) {
     return res.status(404).json({
@@ -94,12 +109,6 @@ const deleteTour = (req, res) => {
     data: null,
   });
 };
-
-//app.get('/api/v1/tours/:id', getAllTours);
-// app.get('/api/v1/tours/:id', getTour);
-// app.post('/api/v1/tours', createTour);
-// app.patch('/api/v1/tours/:id', updateTour);
-// app.delete('/api/v1/tours/:id', deleteTour);
 
 app.route('/api/v1/tours').get(getAllTours).post(createTour);
 
