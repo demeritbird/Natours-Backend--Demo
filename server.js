48f460535df258fe296529a0101129ec1134e420
demeritbird <lekjiewei@hotmail.sg>
2022-08-27 16:11:55 +0800

    naive imple of Create Document

--- server.js
+++ server.js
@@ -20,7 +20,34 @@ mongoose
     console.log('DB connection successful!');
   });
 
-console.log(process.env);
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+});
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  rating: 4.7,
+  price: 497,
+});
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => console.log(err));
 
 const port = 3000;
 app.listen(port, () => {
