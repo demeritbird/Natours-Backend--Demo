cb731ad38a662f255102b42e589807dc2f255f59
demeritbird <lekjiewei@hotmail.sg>
2022-08-28 20:44:34 +0800

    feat: simple script to upload hard data into API

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const Tour = require('./../../models/tourModel');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log('DB connection successful!');
+  });
+
+// Read JSON file
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+// Import Data into Database
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data successfully loaded!');
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// Delete All Data from Collection
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany(); // This deletes all documents since no query.
+    console.log('Data successfully deleted!');
+    process.exit();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
+
+console.log(process.argv);
