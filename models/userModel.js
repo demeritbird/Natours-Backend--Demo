95bd5f3ac556d2addefcc9b56edc24e51e938753
demeritbird <lekjiewei@hotmail.sg>
2022-09-09 06:26:01 +0800

    feat: userModel & createUser

---
+++ models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name!'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email!'],
+    unique: true,
+    lowercase: true, // converts output to lowercase
+    validate: [validator.isEmail, 'Please provide a valid email!'], // checks for email type.
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password!'],
+    minLength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
