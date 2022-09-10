1de2ab81097858a3add4ac3aaa9c6fb6ea1dc451
demeritbird <lekjiewei@hotmail.sg>
2022-09-10 09:26:31 +0800

    Login Users w Authentication/JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,14 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 const jwt = require('jsonwebtoken');
+const AppError = require('./../utils/appError');
+const bcrypt = require('bcryptjs');
+
+function signToken(id) {
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+}
 
 exports.signup = catchAsync(async (req, res, next) => {
   // const newUser = await User.create(req.body);
@@ -11,9 +19,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: 'success',
@@ -21,5 +27,29 @@ exports.signup = catchAsync(async (req, res, next) => {
     data: {
       user: newUser,
     },
+  });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  // read email && password from the request body
+  const { email, password } = req.body;
+
+  // 1) Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password!', 400));
+  }
+  // 2) Check if user existts && password is correct
+  const user = await User.findOne({ email }).select('+password');
+  //const correct = await user.correctPassword(password, user.password);
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password!', 401));
+  }
+
+  const token = signToken(user._id);
+
+  res.status(200).json({
+    status: 'success',
+    token,
   });
 });
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,15 @@
+const User = require('./../models/userModel');
+
 // User Functions
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined!',
+exports.getAllUsers = async (req, res) => {
+  const user = await User.find();
+
+  res.status(200).json({
+    status: 'success',
+    results: user.length,
+    data: {
+      user,
+    },
   });
 };
 exports.createUsers = (req, res) => {
