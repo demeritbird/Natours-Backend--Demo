7cea2a100f57682da8d1e42963b959a28a2e9ad6
demeritbird <lekjiewei@hotmail.sg>
2022-09-18 11:29:11 +0800

    feat: update user info whilst logged in

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,14 @@
 const User = require('./../models/userModel');
+const AppError = require('./../utils/appError');
+const catchAsync = require('./../utils/catchAsync');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 // User Functions
 exports.getAllUsers = async (req, res) => {
@@ -18,6 +28,32 @@ exports.createUsers = (req, res) => {
     message: 'This route is not yet defined!',
   });
 };
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This is not for password updates! Please use /updateMyPassword',
+        400
+      )
+    );
+  }
+  // 2) Update user document
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
+    },
+  });
+});
+
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
