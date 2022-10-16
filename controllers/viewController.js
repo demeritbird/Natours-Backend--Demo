ed8e69361ee9784ce2daa3471c8e1a6f8d02f2c0
demeritbird <lekjiewei@hotmail.sg>
2022-10-16 11:58:59 +0800

    feat: build tour overview page w pug

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,8 +1,18 @@
-exports.getOverview = (req, res) => {
+const catchAsync = require('../utils/catchAsync');
+const Tour = require('./../models/tourModel');
+
+exports.getOverview = catchAsync(async (req, res) => {
+  // 1) Get Tour Ddata from collection
+  const tours = await Tour.find();
+
+  // 2) Build Template
+  // 3) Render that template using tour data from 1)
+
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
