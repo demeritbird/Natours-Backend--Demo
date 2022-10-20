f8d88a105502527917dae204a9ef9ddaa1640626
demeritbird <lekjiewei@hotmail.sg>
2022-10-21 07:40:50 +0800

    feat: user page and further finetuning routes

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -117,3 +117,26 @@ module.exports = (err, req, res, next) => {
     sendErrorProd(error, req, res);
   }
 };
+
+// module.exports = (err, req, res, next) => {
+//   // console.log(err.stack);
+
+//   err.statusCode = err.statusCode || 500;
+//   err.status = err.status || 'error';
+
+//   if (process.env.NODE_ENV === 'development') {
+//     sendErrorDev(err, req, res);
+//   } else if (process.env.NODE_ENV === 'production') {
+//     let error = { ...err };
+//     error.message = err.message;
+
+//     if (error.name === 'CastError') error = handleCastErrorDB(error);
+//     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
+//     if (error.name === 'ValidationError')
+//       error = handleValidationErrorDB(error);
+//     if (error.name === 'JsonWebTokenError') error = handleJWTError();
+//     if (error.name === 'TokenExpiredError') error = handleJWTExpiredError();
+
+//     sendErrorProd(error, req, res);
+//   }
+// };
