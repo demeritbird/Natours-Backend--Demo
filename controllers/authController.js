866f55229e36d37bbf0e018a60edc8db7ad474a3
demeritbird <lekjiewei@hotmail.sg>
2022-10-25 07:55:49 +0800

    feat: nodemailer w templates for signup & forgetPassword

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,6 +6,7 @@ const bcrypt = require('bcryptjs');
 const { promisify } = require('util');
 const sendEmail = require('./../utils/email');
 const crypto = require('crypto');
+const Email = require('./../utils/email');
 
 function signToken(id) {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -47,17 +48,21 @@ exports.signup = catchAsync(async (req, res, next) => {
     photo: req.body.photo,
   });
 
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  console.log(url);
+  await new Email(newUser, url).sendWelcome();
+
   createSendToken(newUser, 201, res);
 
-  const token = signToken(newUser._id);
+  // const token = signToken(newUser._id);
 
-  res.status(201).json({
-    status: 'success',
-    token,
-    data: {
-      user: newUser,
-    },
-  });
+  // res.status(201).json({
+  //   status: 'success',
+  //   token,
+  //   data: {
+  //     user: newUser,
+  //   },
+  // });
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -183,31 +188,67 @@ exports.restrictTo = (...roles) => {
   };
 };
 
+// exports.forgotPassword = catchAsync(async (req, res, next) => {
+//   // 1)Get user based on POST email
+//   const user = await User.findOne({ email: req.body.email });
+//   if (!user) {
+//     return next(new AppError('There is no user with that email address!', 404));
+//   }
+
+//   // 2) Generate the random REST token
+//   const resetToken = user.createPasswordResetToken();
+//   await user.save({ validateBeforeSave: false });
+
+//   // 3) Send it to user's email
+//   const resetURL = `${req.protocol}://${req.get(
+//     'host'
+//   )}/ap1/v1/users/resetPassword/${resetToken}`;
+
+//   const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.
+//   If you didn't forget your password, please ignore this email!`;
+
+//   try {
+//     await sendEmail({
+//       email: user.email,
+//       subject: 'Your password reset token (valid for 10mins)',
+//       message,
+//     });
+
+//     res.status(200).json({
+//       status: 'success',
+//       message: 'Token sent to email!',
+//     });
+//   } catch (err) {
+//     user.passwordResetToken = undefined;
+//     user.passwordResetExpires = undefined;
+//     await user.save({ validateBeforeSave: false });
+
+//     return next(
+//       new AppError(
+//         'There was an error sending the email. Try again later!',
+//         500
+//       )
+//     );
+//   }
+// });
+
 exports.forgotPassword = catchAsync(async (req, res, next) => {
-  // 1)Get user based on POST email
+  // 1) Get user based on POSTed email
   const user = await User.findOne({ email: req.body.email });
   if (!user) {
-    return next(new AppError('There is no user with that email address!', 404));
+    return next(new AppError('There is no user with email address.', 404));
   }
 
-  // 2) Generate the random REST token
+  // 2) Generate the random reset token
   const resetToken = user.createPasswordResetToken();
   await user.save({ validateBeforeSave: false });
 
   // 3) Send it to user's email
-  const resetURL = `${req.protocol}://${req.get(
-    'host'
-  )}/ap1/v1/users/resetPassword/${resetToken}`;
-
-  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.
-  If you didn't forget your password, please ignore this email!`;
-
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10mins)',
-      message,
-    });
+    const resetURL = `${req.protocol}://${req.get(
+      'host'
+    )}/api/v1/users/resetPassword/${resetToken}`;
+    await new Email(user, resetURL).sendPasswordReset();
 
     res.status(200).json({
       status: 'success',
@@ -219,10 +260,8 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
     await user.save({ validateBeforeSave: false });
 
     return next(
-      new AppError(
-        'There was an error sending the email. Try again later!',
-        500
-      )
+      new AppError('There was an error sending the email. Try again later!'),
+      500
     );
   }
 });
